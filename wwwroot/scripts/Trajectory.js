f3ea7d68597f81c0a09f506f18701ea3c73f105c
vnorris <nvsibeeyt@gmail.com>
2022-06-20 16:25:00 +0100

    Close #44. Test trajectory fully configured by mouse dragging. Canvas came #51 started

--- wwwroot/scripts/Trajectory.js
+++ wwwroot/scripts/Trajectory.js
@@ -1,84 +1,152 @@
 // Credit to PhysicsMatters on youtube i.e.  https://twitter.com/MattersPhysics for the tutorial that built this
 
-const canvas = document.createElement("canvas");
+const canvas = document.getElementById("canvas");
 const drawing = canvas.getContext("2d");
-canvas.width = (window.innerWidth) - 15;
-canvas.height = (window.innerHeight / 2) - 15;
+let mouseX = "";
+let mouseY = "";
+let mouseStartX = "";
+let mouseStartY = "";
+let mouseEndX = "";
+let mouseEndY = "";
+let blueLength = "";
 let time = 0;
+let draggingMouse = false;
+let mouseDown = false;
+let ballExists = false;
 
 const xStart = 0;
 let x = xStart;
 
-const yStart = canvas.height; //(0,0) is in the top right
+const yStart = canvas.height - 200; //(0,0) is in the top right
 let y = yStart;
 
-const angle = document.getElementById("angle")
-const speed = document.getElementById("speed")
+let angle = 45
+let angleFromDrag = 0;
+const speed = 5;
 const g = 0.05; //gravitational force [px]
 
 let oldTimestamp = 0;
 var secondsPassed = 0;//between each frame
-var animationSpeed = document.getElementById("animation")// how fast to animate the process. Tick speed
+var animationSpeed = 81
 
 let run = false;
-let start = document.getElementById("start");
+//let start = document.getElementById("start");
 
-start.addEventListener("click", function () {
-    if (!run) {
-        start.value ='stop';
-    } else {
-        start.value = 'start';
-        x = xStart;
-        y = yStart;
-        time = 0;
-    }
-    run = !run
-});
+//start.addEventListener("click", function () {
+//    go()
+//});
 
 function init() {
-    window.requestAnimationFrame(animationLoop);
+    window.requestAnimationFrame(animationLoop);    
 }
 
 function animationLoop(timestamp) {
     secondsPassed = (timestamp - oldTimestamp) / 1000 //ms to seconds
     oldTimestamp = timestamp;//new frame, we need to keep getting the difference between each frame
     update();
+    drawing.clearRect(0, 0, canvas.width, canvas.height);// remove all old objects
+    drawBall();
     draw();
     window.requestAnimationFrame(animationLoop);
 }
 
 function update() {
     if (run) {
-        time += animationSpeed.value * secondsPassed;
-        x = speed.value * Math.cos(-angle.value * Math.PI / 180) * time + xStart;
-        y = .5 * g * time * time + speed.value * Math.sin(-angle.value * Math.PI / 180) * time + yStart;
+        moveBall()
     }
 }
 
-function draw() {
-    drawing.clearRect(0, 0, canvas.width, canvas.height);// remove all old objects
-    drawing.beginPath();
-    drawing.fillStyle = "green";
-    drawing.rect(0, canvas.height, canvas.width, -canvas.height + 200);
-    drawing.fill();
-    drawing.beginPath();
-    drawing.fillStyle = "blue";
-    drawing.rect(0, -canvas.height + 200, canvas.width, canvas.height);
-    drawing.fill();
+function moveBall() {
+    const speed = (Math.round(blueLength / 50) > 5 ? 5 : Math.round(blueLength / 50)) + 3;
+    time += animationSpeed * secondsPassed;
+    x = speed * Math.cos(-angle * Math.PI / 180) * time + xStart;
+    y = .5 * g * time * time + speed * Math.sin(-angle * Math.PI / 180) * time + yStart;
+}
+
+function drawBall() {
     drawing.beginPath();
     drawing.fillStyle = "black";
     drawing.arc(x, y, 10, 0, 2 * Math.PI);
     drawing.fill();
+}
+
+function draw() {
+    drawing.beginPath();
+    drawing.fillStyle = "black";
+    drawing.arc(x, y, 10, 0, 2 * Math.PI);    
+    //drawing.fillText("X: " + mouseX + ", Y: " + mouseY, 10, 20);
+    //drawing.fillText("X: " + mouseStartX + ", Y: " + mouseEndX, 10, 50);
+    //drawing.fillText("ballY:" + y + " canvasY: " + yStart, 10, 80);
+    //drawing.fillText("dragAngle:" + angleFromDrag + " angle: " + angle, 10, 110);
+    //drawing.fillText("blueLengthL " + blueLength, 10, 140);
+    drawing.fill();
     drawing.fillStyle = "white";
-    drawing.fillText("Angle: " + angle.value, 50, 80);
-    drawing.fillText("Speed: " + speed.value, 50, 90);
-    drawing.fillText("Animation speed: " + animationSpeed.value, 50, 100);
+    if (draggingMouse && !ballExists && !hasCollided()) {
+        drawing.beginPath();
+        drawing.moveTo(mouseStartX, mouseStartY);
+        drawing.lineWidth = "10";
+        drawing.strokeStyle = "blue"
+        drawing.lineTo(mouseEndX, mouseEndY);
+        drawing.stroke();
+        blueLength = Math.hypot(mouseEndX - mouseStartX, mouseEndY - mouseStartY)
+    }
     drawing.beginPath();
     drawing.moveTo(xStart, yStart);
-    drawing.strokeStyle = "grey";
-    drawing.lineTo(xStart + 80 * Math.cos(angle.value * Math.PI / 180), yStart - 80 * Math.sin(angle.value * Math.PI / 180));
+    drawing.strokeStyle = "black";
+    drawing.lineTo(xStart + 80 * Math.cos(angle * Math.PI / 180), yStart - 80 * Math.sin(angle * Math.PI / 180));
     drawing.lineWidth = "40";
     drawing.stroke();
+    if (hasCollided()) {
+        go();
+    }
+}
+
+canvas.addEventListener("mousemove", function (e) {        
+    if (draggingMouse && !ballExists && !hasCollided()) {
+        cRect = canvas.getBoundingClientRect();
+        mouseEndX = Math.round(e.clientX - cRect.left);
+        mouseEndY = Math.round(e.clientY - cRect.top);
+        angleFromDrag = Math.round(Math.atan2(mouseEndY - mouseStartY, mouseStartX - mouseEndX) * 180 / Math.PI);
+        angleFromDrag = angleFromDrag > 90 ? 90 : angleFromDrag;
+        angleFromDrag = angleFromDrag < 0 ? 0 : angleFromDrag;
+        angle = angleFromDrag;
+    }
+    if (mouseDown) {
+        draggingMouse = true;
+    }
+});
+
+canvas.addEventListener("mousedown", function (e) {    
+    draggingMouse = false;
+    mouseDown = true;
+    cRect = canvas.getBoundingClientRect();
+    mouseStartX = Math.round(e.clientX - cRect.left);
+    mouseStartY = Math.round(e.clientY - cRect.top);
+});
+
+canvas.addEventListener("mouseup", function (e) {
+    mouseDown = false;
+    if (draggingMouse && !ballExists) {
+        go();
+        draggingMouse = false;
+    }
+});
+
+function go() {
+    if (!run && !ballExists) {
+        ballExists = true;
+        run = !run
+    } else if (hasCollided()) {
+        x = xStart;
+        y = yStart;
+        time = 0;
+        run = !run;
+        ballExists = false;
+    }
+}
+
+function hasCollided() {
+    return y > canvas.height + 10 || x > canvas.width + 10;
 }
 
 document.body.appendChild(canvas);
